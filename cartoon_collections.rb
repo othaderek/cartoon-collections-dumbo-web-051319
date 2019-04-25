6bd3588ab9fe69847b9b1d457bcb16e47b5a14c4
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-25 03:58:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(arr)
   return new_arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.map
 end
 
 def find_the_cheese# code an argument here
