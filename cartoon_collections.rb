c662992790faff94bd9e3521b27dae72146c8adb
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-25 03:17:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(arr)
   arr.each_with_index {|item, index| puts "#{index + 1} #{item}"}
 end
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  
 end
 
 def long_planeteer_calls# code an argument here
