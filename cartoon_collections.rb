5291dd9e34eb1f3b22356fd6daedcc1b39258511
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-25 04:33:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def long_planeteer_calls(arr)
   arr.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
